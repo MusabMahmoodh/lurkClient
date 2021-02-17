f619fd683770dc6f68cf395ec5f6f5d6cfa5c2ea
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-02-17 20:36:29 +0530

    initial setup

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import axios from "axios";
+
+import Product from "../components/Product";
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products/");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} lg={4} xl={3} key={product.id}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export default HomeScreen;
